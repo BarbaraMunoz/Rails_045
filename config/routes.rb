eed10c1f31bb83cfc84826652f0396558872e9f2
Bárbara <barmunozs@gmail.com>
2023-06-15 14:42:35 -0400

    Se agrega bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'contact/index'
-  get 'projects/index'
-  get 'home/index'
+
+  get "contact", to: "contact#index" #controlador#metodo
+  get "projects", to: "projects#index" #controlador#metodo
+  get "home", to: "home#index" #controlador#metodo
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
